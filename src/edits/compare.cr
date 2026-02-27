8b7e66431b7475a990d50b722d0b5052d983cb1a
Tom Crouch <tom.crouch@gmail.com>
2026-02-27 22:11:23 +0000

    refactor: use guard clauses and tighten type hint

--- src/edits/compare.cr
+++ src/edits/compare.cr
@@ -6,13 +6,9 @@ module Edits
     # `Klass.most_similar("foo", strings)` is functionally equivalent to
     # `strings.min_by { |s| Klass.distance("foo", s) }`, leveraging a
     # max distance.
-    def most_similar(prototype, strings : Enumerable)
-      case strings.size
-      when 0 then raise Enumerable::EmptyError.new
-      when 1 then return strings.first
-      else
-        nil
-      end
+    def most_similar(prototype, strings : Indexable)
+      raise Enumerable::EmptyError.new if strings.empty?
+      return strings.first if strings.size == 1
 
       min_s = strings[0]
       min_d = distance(prototype, min_s)
